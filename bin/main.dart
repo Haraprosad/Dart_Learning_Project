eb8304b1bde33319bfdfc2df736c14142eccbd54
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-27 12:47:49 +0600

    1)Switch_Statement

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+import 'package:Dart_Learning_Project/Dart_Learning_Project.dart' as Dart_Learning_Project;
+
+void main(){
+  String testCase = "b";
+  switch(testCase){
+    case "a":
+      print("your awesome man......");
+      break;
+    case "b":
+      print("You are great man.");
+      break;
+    default:
+      print("You are the super boss....");
+  }
+}
---
+++ test/Dart_Learning_Project_test.dart
@@ -0,0 +1,8 @@
+import 'package:Dart_Learning_Project/Dart_Learning_Project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
