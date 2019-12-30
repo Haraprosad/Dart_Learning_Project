eb938b4156c53d0a0ee58148b9e4e6bfb16d6894
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-12-30 11:08:03 +0600

    25)Growable List

---
+++ bin/Growable list.dart
@@ -0,0 +1,25 @@
+void main(){
+  List<int> num = List(); //Growable list declaration
+  //Elements insertion
+  num.add(45);
+  num.add(67);
+  num.add(12);
+
+  //print all the elements
+  num.forEach((i)=>print(i));
+
+  //update elements
+  num[0] = 33;
+  num[1] = 55;
+  num.insert(2, 66);
+  print("After update:");
+  num.forEach((i)=>print(i));
+
+  //delete operation
+  num.remove(12);
+  num.removeAt(0);
+
+  print("Afeter deletation:");
+  num.forEach((i)=>print(i));
+
+}
